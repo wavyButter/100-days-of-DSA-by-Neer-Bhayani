54dd1ab10b6b1267a5c069234a05096a4d1581ee
Neer Bhayani <bhayanineer@gmail.com>
2026-02-11 08:10:14 +0530

    Implement power function to calculate exponentiation

---
+++ d8q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int power(int a,int b){
+        if(b == 0)
+           return 1;
+
+        return a * power(a, b-1);
+    }       
+int main () {
+    int a,b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", power(a, b));
+
+
+    
+    return 0;
+}
